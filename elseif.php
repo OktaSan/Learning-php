8cdf39cd839323953b5ce2227e3b193740c1cc76
Okta San <oktaxd88@gmail.com>
2024-03-02 18:55:47 +0700

    Else-if-elseif
    Penjelasan tentang else, if, elseif.

---
+++ elseif.php
@@ -0,0 +1,17 @@
+<?php
+
+$x = 2;
+$y = 4;
+$z = 5;
+
+if (($x > 1) && ($y < 5) && ($z == 6)) {
+    echo "Benar semua!";
+}
+
+elseif (($x > 1) && ($y < 5) && ($z == 5)) {
+    echo "Benar semua2!";
+}
+
+else {echo "ada yang salah!";}
+
+?>
