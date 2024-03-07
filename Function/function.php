9867afcb39bfc79efc05e408ac321d159e436cbc
Okta San <oktaxd88@gmail.com>
2024-03-08 00:45:46 +0700

    Code_Function
    Menambahkan contoh kode function.

---
+++ Function/function.php
@@ -0,0 +1,13 @@
+<?php
+
+function siswa()
+{
+    $a = "Ling";
+    $b = "Wanwan";
+    echo "Perkenalkan saya adalah $a suami dari $b ";
+    echo "hehehehe.";
+    echo "<br>";
+}
+
+siswa();
+siswa();
